5e6997bf6db5c5df3224965fa83192af8f2e487e
Hiro <laciferin@gmail.com>
2023-05-20 09:53:52 +0530

    dyn import

---
+++ rider-sharing/tests/dyn_import.py
@@ -0,0 +1,20 @@
+import os
+import importlib
+
+src_dir = 'src'  # Specify the path to the src directory
+
+# Iterate over the files in the src directory
+for file in os.listdir(src_dir):
+    if file.endswith('.py'):  # Filter only Python files
+        module_name = file[:-3]  # Extract the module name without the .py extension
+        file_path = os.path.join(src_dir, file)  # Construct the file path
+        spec = importlib.util.spec_from_file_location(module_name, file_path)
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+
+        for name, value in vars(module).items():
+            # callable(value)
+            if name not in globals():
+                globals()[name] = value
+
+imported=True
